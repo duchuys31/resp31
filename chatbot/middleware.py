bade550523c818a0b511f05e2b855bb5faa1df65
duchuys31 <huy52670@gmail.com>
2023-12-09 20:13:56 +0700

    change middleware

--- chatbot/middleware.py
+++ chatbot/middleware.py
@@ -7,18 +7,21 @@ class CustomMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        data = json.loads(request.body)
-        print(data)
         try:
-            customer = Customer.objects.get(sender_id=data['sender_id'])
-        except:
-            customer = Customer.objects.create(sender_id=data['sender_id'])
-        
-        try:
-            customer.language = detech_language(data['sender_input'])['language'] 
-        except: 
-            customer.language = 'Vietnamese'
-        customer.save()
-        request.customer = customer
+            data = json.loads(request.body)
+            print(data)
+            try:
+                customer = Customer.objects.get(sender_id=data['sender_id'])
+            except:
+                customer = Customer.objects.create(sender_id=data['sender_id'])
+            
+            try:
+                customer.language = detech_language(data['sender_input'])['language'] 
+            except: 
+                customer.language = 'Vietnamese'
+            customer.save()
+            request.customer = customer
+        except Exception as e: 
+            print(str(e))
         response = self.get_response(request)
         return response
